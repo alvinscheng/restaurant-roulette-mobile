684046af5857d029a59d41288d15924815c258a0
Alvin Cheng <ascheng21@gmail.com>
2017-09-24 17:36:25 -0700

    Added native-base as dependency. #1

--- App.js
+++ App.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
+import { Button } from 'native-base'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <ScrollView
+          style={styles.container}
+          contentContainerStyle={styles.contentContainer}>
+
+          <View style={styles.getStartedContainer}>
+            <Text style={styles.getStartedText}>
+              Restaurant Roulette
+            </Text>
+          </View>
+
+          <View>
+            <Button bordered info
+              style={styles.getStartedButton}
+              onPress={() =>
+                console.log('hi')
+              }>
+              <Text>Click Me!</Text>
+            </Button>
+          </View>
+
+        </ScrollView>
       </View>
-    );
+    )
   }
 }
 
@@ -15,7 +36,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-});
+  contentContainer: {
+    paddingTop: 30,
+  },
+  getStartedContainer: {
+    alignItems: 'center',
+  },
+  getStartedText: {
+    marginTop: 50,
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'black',
+    textAlign: 'center',
+  },
+  getStartedButton: {
+    marginTop: 20,
+    marginLeft: 140,
+  },
+})
